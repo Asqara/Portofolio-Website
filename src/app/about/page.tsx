6702cd39bbe8ed1caafe3e97d7414a14d2adc809
Asqara <alfath.hayan@gmail.com>
2025-03-27 18:50:39 +0700

    coba test

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar"
+
+export default function page(){
+    return(
+        <div>
+
+        <Navbar />
+        <h1>About</h1>
+        <p>Saya adalah seorang developer yang suka membangun aplikasi dengan teknologi modern.</p>
+        
+        </div>
+    );
+
+}
